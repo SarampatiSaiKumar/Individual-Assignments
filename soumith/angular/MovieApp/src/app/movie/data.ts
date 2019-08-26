11dd9641351f41e640aa2edc1f3d79e84d897a2e
SoumithEdla <soumith8187@gmail.com>
2019-08-26 16:20:43 +0530

    added

---
+++ soumith/angular/MovieApp/src/app/movie/data.ts
@@ -0,0 +1,402 @@
+export  var data 
+=
+[
+  {
+    "vote_count": 407,
+    "id": 429203,
+    "video": false,
+    "vote_average": 6.3,
+    "title": "The Old Man & the Gun",
+    "popularity": 315.75,
+    "poster_path": "/a4BfxRK8dBgbQqbRxPs8kmLd8LG.jpg",
+    "original_language": "en",
+    "original_title": "The Old Man & the Gun",
+    "genre_ids": [
+      35,
+      80,
+      18
+    ],
+    "backdrop_path": "/8bRIfPGDnmWgdy65LO8xtdcFmFP.jpg",
+    "adult": false,
+    "overview": "The true story of Forrest Tucker, from his audacious escape from San Quentin at the age of 70 to an unprecedented string of heists that confounded authorities and enchanted the public. Wrapped up in the pursuit are a detective, who becomes captivated with Forrest’s commitment to his craft, and a woman, who loves him in spite of his chosen profession.",
+    "release_date": "2018-09-27"
+  },
+  {
+    "vote_count": 743,
+    "id": 384018,
+    "video": false,
+    "vote_average": 6.5,
+    "title": "Fast & Furious Presents: Hobbs & Shaw",
+    "popularity": 215.643,
+    "poster_path": "/keym7MPn1icW1wWfzMnW3HeuzWU.jpg",
+    "original_language": "en",
+    "original_title": "Fast & Furious Presents: Hobbs & Shaw",
+    "genre_ids": [
+      28
+    ],
+    "backdrop_path": "/hpgda6P9GutvdkDX5MUJ92QG9aj.jpg",
+    "adult": false,
+    "overview": "A spinoff of The Fate of the Furious, focusing on Johnson's US Diplomatic Security Agent Luke Hobbs forming an unlikely alliance with Statham's Deckard Shaw.",
+    "release_date": "2019-08-01"
+  },
+  {
+    "vote_count": 1931,
+    "id": 420818,
+    "video": false,
+    "vote_average": 7.1,
+    "title": "The Lion King",
+    "popularity": 205.166,
+    "poster_path": "/2bXbqYdUdNVa8VIWXVfclP2ICtT.jpg",
+    "original_language": "en",
+    "original_title": "The Lion King",
+    "genre_ids": [
+      12,
+      16,
+      10751,
+      18,
+      28
+    ],
+    "backdrop_path": "/1TUg5pO1VZ4B0Q1amk3OlXvlpXV.jpg",
+    "adult": false,
+    "overview": "Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.",
+    "release_date": "2019-07-12"
+  },
+  {
+    "vote_count": 1996,
+    "id": 458156,
+    "video": false,
+    "vote_average": 7.1,
+    "title": "John Wick: Chapter 3 – Parabellum",
+    "popularity": 171.1,
+    "poster_path": "/ziEuG1essDuWuC5lpWUaw1uXY2O.jpg",
+    "original_language": "en",
+    "original_title": "John Wick: Chapter 3 – Parabellum",
+    "genre_ids": [
+      80,
+      28,
+      53
+    ],
+    "backdrop_path": "/vVpEOvdxVBP2aV166j5Xlvb5Cdc.jpg",
+    "adult": false,
+    "overview": "Super-assassin John Wick returns with a $14 million price tag on his head and an army of bounty-hunting killers on his trail. After killing a member of the shadowy international assassin’s guild, the High Table, John Wick is excommunicado, but the world’s most ruthless hit men and women await his every turn.",
+    "release_date": "2019-05-15"
+  },
+  {
+    "vote_count": 907,
+    "id": 479455,
+    "video": false,
+    "vote_average": 5.9,
+    "title": "Men in Black: International",
+    "popularity": 145.679,
+    "poster_path": "/dPrUPFcgLfNbmDL8V69vcrTyEfb.jpg",
+    "original_language": "en",
+    "original_title": "Men in Black: International",
+    "genre_ids": [
+      28,
+      35,
+      878,
+      12
+    ],
+    "backdrop_path": "/2FYzxgLNuNVwncilzUeCGbOQzP7.jpg",
+    "adult": false,
+    "overview": "The Men in Black have always protected the Earth from the scum of the universe. In this new adventure, they tackle their biggest, most global threat to date: a mole in the Men in Black organization.",
+    "release_date": "2019-06-12"
+  },
+  {
+    "vote_count": 19,
+    "id": 486589,
+    "video": false,
+    "vote_average": 5.5,
+    "title": "Red Shoes and the Seven Dwarfs",
+    "popularity": 132.549,
+    "poster_path": "/MBiKqTsouYqAACLYNDadsjhhC0.jpg",
+    "original_language": "en",
+    "original_title": "Red Shoes and the Seven Dwarfs",
+    "genre_ids": [
+      16,
+      10749
+    ],
+    "backdrop_path": "/bga3i5jcejBekr2FCGJga1fYCh.jpg",
+    "adult": false,
+    "overview": "Princes who have been turned into Dwarfs seek the red shoes of a lady in order to break the spell, although it will not be easy.",
+    "release_date": "2019-07-25"
+  },
+  {
+    "vote_count": 4,
+    "id": 593961,
+    "video": false,
+    "vote_average": 8.6,
+    "title": "Hello, Love, Goodbye",
+    "popularity": 130.718,
+    "poster_path": "/kTca5fpKhFOKIiG0tz8tynhsWs5.jpg",
+    "original_language": "tl",
+    "original_title": "Hello, Love, Goodbye",
+    "genre_ids": [
+      10749,
+      18
+    ],
+    "backdrop_path": "/foMDjFHrTrex6Rr6bApB8TbWlie.jpg",
+    "adult": false,
+    "overview": "A love story of Joy and Ethan, Filipino workers based in Hong Kong. Ethan, a bartender, is keen on romantically pursuing Joy, a domestic helper who is wholly dedicated to providing for her family.",
+    "release_date": "2019-07-31"
+  },
+  {
+    "vote_count": 8889,
+    "id": 299534,
+    "video": false,
+    "vote_average": 8.4,
+    "title": "Avengers: Endgame",
+    "popularity": 129.157,
+    "poster_path": "/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+    "original_language": "en",
+    "original_title": "Avengers: Endgame",
+    "genre_ids": [
+      12,
+      878,
+      28
+    ],
+    "backdrop_path": "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
+    "adult": false,
+    "overview": "After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.",
+    "release_date": "2019-04-24"
+  },
+  {
+    "vote_count": 28,
+    "id": 423204,
+    "video": false,
+    "vote_average": 6.8,
+    "title": "Angel Has Fallen",
+    "popularity": 128.603,
+    "poster_path": "/fapXd3v9qTcNBTm39ZC4KUVQDNf.jpg",
+    "original_language": "en",
+    "original_title": "Angel Has Fallen",
+    "genre_ids": [
+      28
+    ],
+    "backdrop_path": "/k2WyDw2NTUIWnuEs5gT7wgrCQg6.jpg",
+    "adult": false,
+    "overview": "Secret Service Agent Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+    "release_date": "2019-08-21"
+  },
+  {
+    "vote_count": 7901,
+    "id": 920,
+    "video": false,
+    "vote_average": 6.7,
+    "title": "Cars",
+    "popularity": 125.089,
+    "poster_path": "/jpfkzbIXgKZqCZAkEkFH2VYF63s.jpg",
+    "original_language": "en",
+    "original_title": "Cars",
+    "genre_ids": [
+      16,
+      12,
+      35,
+      10751
+    ],
+    "backdrop_path": "/a1MlbLBk5Sy6YvMbSuKfwGlDVlb.jpg",
+    "adult": false,
+    "overview": "Lightning McQueen, a hotshot rookie race car driven to succeed, discovers that life is about the journey, not the finish line, when he finds himself unexpectedly detoured in the sleepy Route 66 town of Radiator Springs. On route across the country to the big Piston Cup Championship in California to compete against two seasoned pros, McQueen gets to know the town's offbeat characters.",
+    "release_date": "2006-06-08"
+  },
+  {
+    "vote_count": 64,
+    "id": 521777,
+    "video": false,
+    "vote_average": 6.8,
+    "title": "Good Boys",
+    "popularity": 123.828,
+    "poster_path": "/jIthqo2tQmW8TFN1fYpF8FmVL0o.jpg",
+    "original_language": "en",
+    "original_title": "Good Boys",
+    "genre_ids": [
+      35
+    ],
+    "backdrop_path": "/6Xsz9KHQmCcIcj3CoWQq5eLtVoT.jpg",
+    "adult": false,
+    "overview": "A group of young boys on the cusp of becoming teenagers embark on an epic quest in the San Fernando Valley to fix their broken toy before their parents get home.",
+    "release_date": "2019-07-18"
+  },
+  {
+    "vote_count": 1096,
+    "id": 373571,
+    "video": false,
+    "vote_average": 6.2,
+    "title": "Godzilla: King of the Monsters",
+    "popularity": 118.33,
+    "poster_path": "/pU3bnutJU91u3b4IeRPQTOP8jhV.jpg",
+    "original_language": "en",
+    "original_title": "Godzilla: King of the Monsters",
+    "genre_ids": [
+      878,
+      28
+    ],
+    "backdrop_path": "/cNt14e43I2DDW6Xd9zFhrP8eOcA.jpg",
+    "adult": false,
+    "overview": "Follows the heroic efforts of the crypto-zoological agency Monarch as its members face off against a battery of god-sized monsters, including the mighty Godzilla, who collides with Mothra, Rodan, and his ultimate nemesis, the three-headed King Ghidorah. When these ancient super-species - thought to be mere myths - rise again, they all vie for supremacy, leaving humanity's very existence hanging in the balance.",
+    "release_date": "2019-05-29"
+  },
+  {
+    "vote_count": 55,
+    "id": 390634,
+    "video": false,
+    "vote_average": 5.3,
+    "title": "Fate/stay night: Heaven’s Feel II. lost butterfly",
+    "popularity": 116.974,
+    "poster_path": "/4tS0iyKQBDFqVpVcH21MSJwXZdq.jpg",
+    "original_language": "ja",
+    "original_title": "劇場版 Fate／stay night [Heaven's Feel] II. lost butterfly",
+    "genre_ids": [
+      16,
+      28,
+      14,
+      12
+    ],
+    "backdrop_path": "/jFIXVCCGvS20mIFXqDAlVR5KXew.jpg",
+    "adult": false,
+    "overview": "Theatrical-release adaptation of the visual novel \"Fate/stay night\", following the third and final route. (Part 2 of a trilogy.)",
+    "release_date": "2019-01-12"
+  },
+  {
+    "vote_count": 3121,
+    "id": 429617,
+    "video": false,
+    "vote_average": 7.8,
+    "title": "Spider-Man: Far from Home",
+    "popularity": 105.616,
+    "poster_path": "/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg",
+    "original_language": "en",
+    "original_title": "Spider-Man: Far from Home",
+    "genre_ids": [
+      28,
+      12,
+      878
+    ],
+    "backdrop_path": "/dihW2yTsvQlust7mSuAqJDtqW7k.jpg",
+    "adult": false,
+    "overview": "Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.",
+    "release_date": "2019-06-28"
+  },
+  {
+    "vote_count": 22,
+    "id": 571627,
+    "video": false,
+    "vote_average": 3,
+    "title": "The Divine Fury",
+    "popularity": 99.098,
+    "poster_path": "/9Z2qT9iZYLzzsCSYu7A4SEQsKX0.jpg",
+    "original_language": "ko",
+    "original_title": "사자",
+    "genre_ids": [
+      28,
+      27,
+      9648
+    ],
+    "backdrop_path": "/4MDYpCwqSVO5RcTiZ4GEfePzDdl.jpg",
+    "adult": false,
+    "overview": "Yong-hu, a world champion martial artist who believes in no God, only himself, suddenly develops stigmata on his palms. Visiting a church in hopes of healing the stigmata, he ends up saving Father Ahn, who falls into danger while performing an exorcism. In this way he learns of the power of the stigmata. As time passes Yong-hu accepts his new calling and begins saving people who are possessed by demons, but then he must prepare for a final confrontation with the evil Jisin, who ensnares people with the power of the devil.",
+    "release_date": "2019-07-31"
+  },
+  {
+    "vote_count": 938,
+    "id": 504608,
+    "video": false,
+    "vote_average": 7.5,
+    "title": "Rocketman",
+    "popularity": 93.125,
+    "poster_path": "/f4FF18ia7yTvHf2izNrHqBmgH8U.jpg",
+    "original_language": "en",
+    "original_title": "Rocketman",
+    "genre_ids": [
+      10402,
+      18
+    ],
+    "backdrop_path": "/oAr5bgf49vxga9etWoQpAeRMvhp.jpg",
+    "adult": false,
+    "overview": "The story of Elton John's life, from his years as a prodigy at the Royal Academy of Music through his influential and enduring musical partnership with Bernie Taupin.",
+    "release_date": "2019-05-22"
+  },
+  {
+    "vote_count": 2801,
+    "id": 420817,
+    "video": false,
+    "vote_average": 7.1,
+    "title": "Aladdin",
+    "popularity": 90.457,
+    "poster_path": "/yYWGCAerbVAHyfuGBCHKzWJdEtj.jpg",
+    "original_language": "en",
+    "original_title": "Aladdin",
+    "genre_ids": [
+      12,
+      14,
+      10749,
+      35,
+      10751
+    ],
+    "backdrop_path": "/v4yVTbbl8dE1UP2dWu5CLyaXOku.jpg",
+    "adult": false,
+    "overview": "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+    "release_date": "2019-05-22"
+  },
+  {
+    "vote_count": 6,
+    "id": 577970,
+    "video": false,
+    "vote_average": 6,
+    "title": "Dilan 1991",
+    "popularity": 90.348,
+    "poster_path": "/gZX9F8Q9QUAKAsGAotlH1fVwHFq.jpg",
+    "original_language": "id",
+    "original_title": "Dilan 1991",
+    "genre_ids": [
+      10749,
+      18
+    ],
+    "backdrop_path": "/6kFFYxGNi0sRbjKGIHn2Wev1pnb.jpg",
+    "adult": false,
+    "overview": "Dilan & Milea are now dating. But as time goes by, Dilan has to choose between his motorcycle club or Milea.",
+    "release_date": "2019-02-28"
+  },
+  {
+    "vote_count": 430,
+    "id": 412117,
+    "video": false,
+    "vote_average": 6.6,
+    "title": "The Secret Life of Pets 2",
+    "popularity": 88.336,
+    "poster_path": "/nYcaCNkB4EgVyvrXxxbklefDrGL.jpg",
+    "original_language": "en",
+    "original_title": "The Secret Life of Pets 2",
+    "genre_ids": [
+      12,
+      16,
+      35,
+      10751
+    ],
+    "backdrop_path": "/etaok7q2E5tV36oXe7GQzhUQ4fX.jpg",
+    "adult": false,
+    "overview": "Max the terrier must cope with some major life changes when his owner gets married and has a baby. When the family takes a trip to the countryside, nervous Max has numerous run-ins with canine-intolerant cows, hostile foxes and a scary turkey. Luckily for Max, he soon catches a break when he meets Rooster, a gruff farm dog who tries to cure the lovable pooch of his neuroses.",
+    "release_date": "2019-05-24"
+  },
+  {
+    "vote_count": 198,
+    "id": 506574,
+    "video": false,
+    "vote_average": 7.7,
+    "title": "Descendants 3",
+    "popularity": 86.823,
+    "poster_path": "/7IRy0iHdaS0JI3ng4ZYlk5gLSFn.jpg",
+    "original_language": "en",
+    "original_title": "Descendants 3",
+    "genre_ids": [
+      12,
+      10751,
+      10402
+    ],
+    "backdrop_path": "/gzdxeOiO76hDoOSKYCxegj7bk4T.jpg",
+    "adult": false,
+    "overview": "The teenagers of Disney's most infamous villains return to the Isle of the Lost to recruit a new batch of villainous offspring to join them at Auradon Prep.",
+    "release_date": "2019-08-09"
+  }
+]
+;
