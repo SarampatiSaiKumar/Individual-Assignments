614d281cbf91a5c7c0b7a5c1eb0b8c74aa9b16bf
unknown <aashishreddy5555@gmail.com>
2019-08-30 14:05:47 +0530

    files added

--- Aashish/typescriptAssignment/movie/Main.ts
+++ Aashish/typescriptAssignment/movie/Main.ts
@@ -3,12 +3,14 @@ import { Movie } from './Movie';
 
 var Movies : Movie[]=[];
 var i;
+
 for( i=0;i<data.length;i++){
     var m =data[i]
     var mv:Movie= new Movie(m.title,m.overview,m.release_date ,m.id,m.vote_average,m.poster_path);
    
     Movies.push(mv);
 }
+
 for( i=0;i<Movies.length;i++){
     var mv:Movie =Movies[i]
     
